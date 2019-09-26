13086f6d143fbb1ab59d547fc7a83a483cb9d391
Tigran-Melikyan <tigran_meliqyan@mail.ru>
2019-09-26 19:26:54 +0400

    to do list

---
+++ to-do-list/todo.js
@@ -0,0 +1,34 @@
+let close = document.getElementsByClassName("close");
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("add some job");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
